b9d7abdc25d6386b9e6ca3e32238854a17f1c5ce
Jesse CreateThis <jesse@createthis.com>
2017-05-24 16:38:41 -0400

    Feature #2003: Added PanelContainerFactory and Editor.

---
+++ Assets/CreateThis/Scripts/Factory/VR/UI/Container/Editor/PanelContainerFactoryEditor.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace CreateThis.Factory.VR.UI.Container {
+    [CustomEditor(typeof(PanelContainerFactory))]
+    [CanEditMultipleObjects]
+    public class PanelContainerFactoryEditor : ContainerBaseFactoryEditor {
+        SerializedProperty minWidth;
+        SerializedProperty minHeight;
+
+        protected override void OnEnable() {
+            base.OnEnable();
+            minWidth = serializedObject.FindProperty("minWidth");
+            minHeight = serializedObject.FindProperty("minHeight");
+        }
+
+        protected override void BuildGenerateButton() {
+            if (GUILayout.Button("Generate")) {
+                if (target.GetType() == typeof(PanelContainerFactory)) {
+                    PanelContainerFactory containerFactory = (PanelContainerFactory)target;
+                    containerFactory.Generate();
+                }
+            }
+        }
+
+        protected override void AdditionalProperties() {
+            EditorGUILayout.PropertyField(minWidth);
+            EditorGUILayout.PropertyField(minHeight);
+        }
+    }
+}
---
+++ Assets/CreateThis/Scripts/Factory/VR/UI/Container/PanelContainerFactory.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using CreateThis.VR.UI.Container;
+
+namespace CreateThis.Factory.VR.UI.Container {
+    public class PanelContainerFactory : ContainerBaseFactory {
+        public float minWidth;
+        public float minHeight;
+
+        protected override void AddContainer(GameObject target) {
+            PanelContainer container = SafeAddComponent<PanelContainer>(target);
+            target.name = "Panel";
+            container.minWidth = minWidth;
+            container.minHeight = minHeight;
+        }
+    }
+}
