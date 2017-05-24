122bdd4a8567aae37738b18548912562a24932d9
Jesse CreateThis <jesse@createthis.com>
2017-05-24 16:19:42 -0400

    Feature #2002: ColumnContainerFactory

---
+++ Assets/CreateThis/Scripts/Factory/VR/UI/Container/ColumnContainerFactory.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using CreateThis.VR.UI.Container;
+
+namespace CreateThis.Factory.VR.UI.Container {
+    public class ColumnContainerFactory : ContainerBaseFactory {
+        protected override void AddContainer(GameObject target) {
+            ColumnContainer container = SafeAddComponent<ColumnContainer>(target);
+            target.name = "Column";
+            container.padding = padding;
+            container.spacing = spacing;
+        }
+    }
+}
---
+++ Assets/CreateThis/Scripts/Factory/VR/UI/Container/Editor/ColumnContainerFactoryEditor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace CreateThis.Factory.VR.UI.Container {
+    [CustomEditor(typeof(ColumnContainerFactory))]
+    [CanEditMultipleObjects]
+    public class ColumnContainerFactoryEditor : ContainerBaseFactoryEditor {
+        protected override void BuildGenerateButton() {
+            if (GUILayout.Button("Generate")) {
+                if (target.GetType() == typeof(ColumnContainerFactory)) {
+                    ColumnContainerFactory containerFactory = (ColumnContainerFactory)target;
+                    containerFactory.Generate();
+                }
+            }
+        }
+    }
+}
