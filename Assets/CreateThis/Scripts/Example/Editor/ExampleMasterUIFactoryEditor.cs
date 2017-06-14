fc8595af9f7382836fd019221c78f4d4b78fe776
Jesse CreateThis <jesse@createthis.com>
2017-06-14 11:08:39 -0400

    Add ExampleMasterUIFactory to tidy up the non-vrtk scene.

---
+++ Assets/CreateThis/Scripts/Example/Editor/ExampleMasterUIFactoryEditor.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEditor;
+using CreateThis.Factory;
+
+namespace CreateThis.Example {
+    [CustomEditor(typeof(ExampleMasterUIFactory))]
+    [CanEditMultipleObjects]
+
+    public class MMVR_MasterUIFactoryEditor : BaseFactoryEditor {
+        SerializedProperty parent;
+        SerializedProperty skyboxManager;
+        SerializedProperty touchPadMenuController;
+
+        protected override void OnEnable() {
+            base.OnEnable();
+            parent = serializedObject.FindProperty("parent");
+            skyboxManager = serializedObject.FindProperty("skyboxManager");
+            touchPadMenuController = serializedObject.FindProperty("touchPadMenuController");
+        }
+
+        protected override void BuildGenerateButton() {
+            // Take out this if statement to set the value using setter when ever you change it in the inspector.
+            // But then it gets called a couple of times when ever inspector updates
+            // By having a button, you can control when the value goes through the setter and getter, your self.
+            if (GUILayout.Button("Generate")) {
+                if (target.GetType() == typeof(ExampleMasterUIFactory)) {
+                    ExampleMasterUIFactory factory = (ExampleMasterUIFactory)target;
+                    factory.Generate();
+                }
+            }
+        }
+
+        protected override void AdditionalProperties() {
+            base.AdditionalProperties();
+            EditorGUILayout.PropertyField(parent);
+            EditorGUILayout.PropertyField(skyboxManager);
+            EditorGUILayout.PropertyField(touchPadMenuController);
+        }
+    }
+}
---
+++ Assets/CreateThis/Scripts/Example/ExampleMasterUIFactory.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using CreateThis.Unity;
+using CreateThis.Factory;
+using CreateThis.Factory.VR.UI;
+using CreateThis.Factory.VR.UI.File;
+using CreateThis.VR.UI;
+using CreateThis.VR.UI.File;
+using CreateThis.VR.UI.Panel;
+using CreateThis.VR.UI.Controller;
+
+namespace CreateThis.Example {
+    public class ExampleMasterUIFactory : BaseFactory {
+        public GameObject parent;
+        public SkyboxManager skyboxManager;
+        public TouchPadMenuController touchPadMenuController;
+
+        private GameObject disposable;
+        private GameObject keyboardInstance;
+        private Keyboard keyboard;
+        private GameObject fileSaveAsInstance;
+        private FileSaveAs fileSaveAs;
+        private GameObject fileOpenInstance;
+        private FileOpen fileOpen;
+        private GameObject toolsInstance;
+
+        private GameObject CreateKeyboard() {
+            KeyboardFactory factory = Undoable.AddComponent<KeyboardFactory>(disposable);
+            factory.parent = parent;
+            GameObject panel = factory.Generate();
+            keyboardInstance = panel;
+            keyboard = keyboardInstance.GetComponent<Keyboard>();
+            return panel;
+        }
+
+        private GameObject CreateFileSaveAs() {
+            FileSaveAsFactory factory = Undoable.AddComponent<FileSaveAsFactory>(disposable);
+            factory.parent = parent;
+            factory.keyboard = keyboard;
+            GameObject panel = factory.Generate();
+            fileSaveAsInstance = panel;
+            fileSaveAs = fileSaveAsInstance.transform.Find("DrivesPanel").GetComponent<FileSaveAs>();
+            return panel;
+        }
+
+        private GameObject CreateFileOpen() {
+            FileOpenFactory factory = Undoable.AddComponent<FileOpenFactory>(disposable);
+            factory.parent = parent;
+            GameObject panel = factory.Generate();
+            fileOpenInstance = panel;
+            fileOpen = fileOpenInstance.transform.Find("DrivesPanel").GetComponent<FileOpen>();
+            return panel;
+        }
+
+        private GameObject CreateToolsPanel() {
+            ToolsExamplePanelFactory factory = Undoable.AddComponent<ToolsExamplePanelFactory>(disposable);
+            factory.parent = parent;
+            factory.fileOpen = fileOpen;
+            factory.fileSaveAs = fileSaveAs;
+            factory.skyboxManager = skyboxManager;
+            GameObject panel = factory.Generate();
+            toolsInstance = panel;
+
+            var touchPadButtons = touchPadMenuController.touchPadButtons;
+            touchPadButtons[0].onSelected.RemoveAllListeners();
+            touchPadButtons[0].onSelected.AddListener(toolsInstance.GetComponent<StandardPanel>().ToggleVisible);
+            touchPadMenuController.touchPadButtons = touchPadButtons;
+
+            return panel;
+        }
+
+        private void CreateDisposable() {
+            if (disposable) return;
+            disposable = EmptyChild(parent, "disposable");
+        }
+
+        private void CleanParent() {
+            var children = new List<GameObject>();
+            foreach (Transform child in parent.transform) children.Add(child.gameObject);
+            children.ForEach(child => Undo.DestroyObjectImmediate(child));
+        }
+
+        public override GameObject Generate() {
+            base.Generate();
+
+#if UNITY_EDITOR
+            Undo.SetCurrentGroupName("MasterFactory Generate");
+            int group = Undo.GetCurrentGroup();
+
+            Undo.RegisterCompleteObjectUndo(this, "MasterFactory state");
+#endif
+            CleanParent();
+
+            CreateDisposable();
+            CreateKeyboard();
+            CreateFileSaveAs();
+            CreateFileOpen();
+            CreateToolsPanel();
+
+#if UNITY_EDITOR
+            Undo.DestroyObjectImmediate(disposable);
+            Undo.CollapseUndoOperations(group);
+#else
+            Destroy(disposable);
+#endif
+            return parent;
+        }
+    }
+}
